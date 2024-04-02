f97a768b6018f63d45574e1eede24d7ec2530ae4
AlcatrazLY <dixonramirez834@gmail.com>
2024-04-02 15:23:29 -0600

    feat: :sparkles: app.inirServer added to lambda handler
    app.initServer added to lambda handler

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = require('./config/app');
 const serverless = require('serverless-http');
 
 
-app.initServer();
+
 
 exports.handler = async () => {
+  app.initServer();
   return {
     statusCode: 200,
     body: 'Servidor Express en ejecución',
